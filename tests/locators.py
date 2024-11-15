df5d6222390c5efc02e073a5e4610894b4fe2706
Aleksandr Zhukov <hyperpugs@yandex.ru>
2024-11-15 14:59:49 +0300

    locator email now good

--- tests/locators.py
+++ tests/locators.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 
 # Локаторы для страницы регистрации
 REGISTER_NAME_INPUT = (By.XPATH, "//input[@name='name']")  # Поле ввода имени на странице регистрации
-REGISTER_EMAIL_INPUT = (By.XPATH, "//label[contains(text(), 'Email')]") # Поле ввода Email на странице регистрации
+REGISTER_EMAIL_INPUT = (By.XPATH, "//fieldset[2]/div/div/input") # Поле ввода Email на странице регистрации
 REGISTER_PASSWORD_INPUT = (By.XPATH, "//input[@name='Пароль']")  # Поле ввода пароля на странице регистрации
 REGISTER_SUBMIT_BUTTON = (By.XPATH, "//button[text()='Зарегистрироваться']")  # Кнопка отправки формы регистрации
 REGISTER_ERROR_MESSAGE = (By.CLASS_NAME, "input__error")  # Сообщение об ошибке
